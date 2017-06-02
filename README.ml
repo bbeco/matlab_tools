5e01c5120f8c29e7505978daf0fc62a5e4549344
Andrea Beconcini <andrea.beconcini@gmail.com>
2017-06-02 17:02:49 +0200

    Readme file added

---
+++ README.ml
@@ -0,0 +1,9 @@
+This repo contains a set of tools used for my thesis
+
+        -cartesian2LL.m: project a point on the unit sphere and
+                returns its coordinate;
+        -LL2Cartesian.m: does the converse of the previous one;
+        -rotateLL.m: rotate a lat/long encoded image accordingly to
+         the angles provided;
+        -surf_features_avg: computes some statistics on the number of
+                features found in set of images;
