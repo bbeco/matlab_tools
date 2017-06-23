1b332f3cb38d54f2d0f2718cedc8a7a3730cf12b
Andrea Beconcini <andrea.beconcini@gmail.com>
2017-06-23 15:29:51 +0200

    Copiright added to key point mach

--- keyPointsMatch.m
+++ keyPointsMatch.m
@@ -1,3 +1,21 @@
+%	Test script for features matching
+%
+%	Copyright 2017 Andrea Beconcini
+%
+%	This program is free software: you can redistribute it and/or modify
+%	it under the terms of the GNU Lesser General Public License as published by
+%	the Free Software Foundation, either version 3 of the License, or
+%	any later version.
+% 
+%	This program is distributed in the hope that it will be useful,
+%	but WITHOUT ANY WARRANTY; without even the implied warranty of
+%	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%	GNU Lesser General Public License for more details.
+% 
+%	You should have received a copy of the GNU Lesser General Public License
+%	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+
 I1 = imread('~/syntetic_scene2/0001.png');
 I2 = imread('~/syntetic_scene2/0050.png');
 
@@ -10,7 +28,7 @@ points2 = detectSURFFeatures(I2);
 [features2, vpts2] = extractFeatures(I2, points2);
 
 % Feature points matching uses:
-%	Exhaustive, all pairwise checks performed;
+%	Exhaustive, all pairwise checks are performed;
 %	Default match threshold, 1.0 for SURF
 %	Default maxRatios, 0.6 for ambiguous matches
 %	Default matching metric, SAD
