89d202e4a0269c70dee8f4a55e07e3e131e9c881
Andrea Beconcini <andrea.beconcini@gmail.com>
2017-08-22 16:16:43 +0200

    Fixed error in orientatinos2euler
    This function translate the set of orientations in ZYX euler angle
    vector. The orientations had to be transposed before passing them to
    rotm2eul.

--- utils/orientations2euler.m
+++ utils/orientations2euler.m
@@ -2,6 +2,6 @@ function translatedOrient = orientations2euler(orientation)
 	len = size(orientation, 1);
 	translatedOrient = cell(len, 1);
 	for i = 1:len
-		translatedOrient{i} = rotm2eul(orientation{i});
+		translatedOrient{i} = rotm2eul(orientation{i}');
 	end
 end
