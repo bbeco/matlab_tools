2a80836b33500363e2beb32fd488923bc3ae6a45
Andrea Beconcini <andrea.beconcini@gmail.com>
2017-09-11 18:06:46 +0200

    New seq filter implemented. Not tested yet
    The new filter has one mode only. It computes the distances between
    matching features in two images, takes a specific quantile and computes
    the median. If it is above a threshold, the frame is used for pose
    estimation, otherwise it is discarded.
    
    The main function now converts to gray scale only the images that it is
    going to use (not every image in the data folder).

--- filters/selectFrame.m
+++ filters/selectFrame.m
@@ -1,44 +1,47 @@
-function [validFrame, goodMatches] = selectFrame(...
-	llPoints1, llPoints2, angularThreshold, filteringMode)
+function validFrame = selectFrame(...
+	llPoints1, llPoints2, angularThreshold, width, height, quantileAnalysed)
 
+	if ~isnumeric(llPoints1)
+		llPoints1 = llPoints1.Location;
+	end
+	
+	if ~isnumeric(llPoints2)
+		llPoints2 = llPoints2.Location;
+	end
+	
 	distances = 180/pi*computeAngularDistance(...
-			llPoints1, llPoints2, height, width);
-	if nargout > 1
-		goodMatches = distances > angularThreshold;
+			llPoints1, llPoints2, width, height);
+	
+	if nargin < 6
+		% Consider the 80th-percentile of the distances
+		quantileAnalysed = 0.8;
 	end
-		
-	if filteringMode == 1
-		% Mode 1: Discard the corresponding points that are closer than
-		% angularThreshold
-		validFrame = true;
-	elseif filteringMode == 2
-		% Mode 2: Discard the whole frame if the average angular distance
-		% between matches is below angularThreshold
-		validFrame = mean(distances, 1) > angularThreshold;
-	elseif filteringMode == 3
-		% Mode 2: Discard the whole frame if the average angular distance
-		% between matches is below angularThreshold
-		validFrame = median(distances, 1) > angularThreshold;
-	end
+	
+	% extract the 80th-percentile of distances
+	q = quantile(distances, quantileAnalysed);
+	% analyse only distances above q
+	distances = distances(distances >= q);
+	% take the median for robustness
+	validFrame = median(distances) >= angularThreshold;
 end
 
 function angularDistance = computeAngularDistance(...
-	imagePoint1, imagePoint2)
+	imagePoint1, imagePoint2, width, height)
 	len = size(imagePoint1, 1);
 	angularDistance = zeros(len, 1);
 
 	for i = 1:len
 		[lat1, long1] = extractLLCoordinateFromImage(...
-			llPoints1(i, :), height, width);
+			imagePoint1(i, 1), imagePoint1(i, 2), width, height);
 		[lat2, long2] = extractLLCoordinateFromImage(...
-			llPoints2(i, :), height, width);
+			imagePoint2(i, 1), imagePoint2(i, 2), width, height);
 
-		x1 = ll2cartesian(lat1, long1);
-		x2 = ll2cartesian(lat2, long2);
+		x1 = LL2Cartesian(lat1, long1);
+		x2 = LL2Cartesian(lat2, long2);
 
 		d = norm(x1 - x2);
-		% the following remap d on the interval [-1, 1]. The result belongs to
+		% the following remaps d on the interval [-1, 1]. The result belongs to
 		% [-pi/2, pi/2] so we have to add pi/2 for our purpose.
-		angularDistance(i) = 2*asin((d - 1)/2) + pi/2;
+		angularDistance(i) = 2*asin(d/4);
 	end
 end
