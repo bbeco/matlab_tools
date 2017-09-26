50ade87feba332c664fcd4d48ac934bee1362750
Andrea Beconcini <andrea.beconcini@gmail.com>
2017-09-26 20:17:36 +0200

    GT utility functions commented.

--- ground_truth/alignOrientation.m
+++ ground_truth/alignOrientation.m
@@ -1,4 +1,28 @@
 function poses = alignOrientation(poses)
+%	This function aligns every poses so that the first camera's orientation is
+%	eye(3).
+%	
+%	Input:
+%		-poses: the table with camera's locations and orientations.
+%
+%	Output:
+%		poses: The rotated set of poses.
+%
+%	Copyright 2017 Andrea Beconcini
+%
+%	This program is free software: you can redistribute it and/or modify
+%	it under the terms of the GNU Lesser General Public License as published by
+%	the Free Software Foundation, either version 3 of the License, or
+%	any later version.
+% 
+%	This program is distributed in the hope that it will be useful,
+%	but WITHOUT ANY WARRANTY; without even the implied warranty of
+%	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%	GNU Lesser General Public License for more details.
+% 
+%	You should have received a copy of the GNU Lesser General Public License
+%	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
 	R = poses.Orientation{1}';
 	if isequal(poses.Orientation{1}, eye(3))
 		return;
--- ground_truth/translateLocation.m
+++ ground_truth/translateLocation.m
@@ -1,4 +1,28 @@
 function poses = translateLocation(poses)
+%	This function translates every poses so that the first camera's location 
+%	is eye(3).
+%	
+%	Input:
+%		-poses: the table with camera's locations and orientations.
+%
+%	Output:
+%		poses: The rotated set of poses.
+%
+%	Copyright 2017 Andrea Beconcini
+%
+%	This program is free software: you can redistribute it and/or modify
+%	it under the terms of the GNU Lesser General Public License as published by
+%	the Free Software Foundation, either version 3 of the License, or
+%	any later version.
+% 
+%	This program is distributed in the hope that it will be useful,
+%	but WITHOUT ANY WARRANTY; without even the implied warranty of
+%	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%	GNU Lesser General Public License for more details.
+% 
+%	You should have received a copy of the GNU Lesser General Public License
+%	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
 	t = poses.Location{1};
 	
 	if isequal(t, [0 0 0])
