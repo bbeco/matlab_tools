39614e4485caae1340cfef27ab8f7661a00171ce
Andrea Beconcini <andrea.beconcini@gmail.com>
2017-09-29 14:26:37 +0200

    Test case for patch created.

---
+++ test/patchTest.m
@@ -0,0 +1,24 @@
+clear 
+addpath('utils');
+addpath('dense_reconstruction');
+addpath('coordinate_transform');
+img = rgb2gray(...
+	imread(fullfile('images/densification_test/test1/rec01_gt.png')));
+[height, width] = size(img);
+patch = createPatch(img, 70*pi/180, 0, width, height);
+figure
+subplot(2, 1, 1);
+imshow(img);
+ax = subplot(2, 1, 2);
+imshow(patch{1});
+
+while true
+	[x, y, button] = ginput(1);
+	if button == 27
+		break;
+	end
+	round([x, y]);
+	[lat, long] = extractLLCoordinateFromImage(x, y, width, height);
+	patch = createPatch(img, lat, long, width, height);
+	imshow(patch{1}, 'Parent', ax);
+end
--- utils/ll2equirectangular.m
+++ utils/ll2equirectangular.m
@@ -2,7 +2,7 @@ function [u, v] = ll2equirectangular(lat, long, width, height)
 	%LL2EQUIRECTANGULAR convert ll coordinates to image coordinates
 	%   convert the LL coordinate to image coordinate for equirectangular
 	%   mapping.
-	u = (long + pi)/(2*pi)*width;
-	v = (pi/2 - lat)/pi*height;
+	u = min(width, ceil((long + pi)/(2*pi)*width));
+	v = min(height, ceil((pi/2 - lat)/pi*height));
 end
 
