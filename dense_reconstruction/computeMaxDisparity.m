0917a7c9fab0a3311226fa86e4125e8faac5ab8b
Andrea Beconcini <andrea.beconcini@gmail.com>
2017-10-25 17:50:08 +0200

    computeMaxDisparity added.
    The new function detects and matches features in an image pair and
    returns the maximum vertical distance between corresponding features

---
+++ dense_reconstruction/computeMaxDisparity.m
@@ -0,0 +1,17 @@
+function dm_maxDisparity = computeMaxDisparity(gray1,gray2)
+%COMPUTEMAXDISPARITY detects and matches features in the given images and
+	%returns the maximum disparity value.
+points1 = detectSURFFeatures(gray1);
+points2 = detectSURFFeatures(gray2);
+
+features1 = extractFeatures(gray1, points1, 'Upright', true);
+features2 = extractFeatures(gray2, points2, 'Upright', true);
+
+indexPairs = matchFeatures(features1, features2);
+
+distances = abs(cat(1, points1(indexPairs(:, 1))) - ...
+	cat(1, points2(indexPairs(:, 2))));
+
+dm_maxDisparity = max(distances(:, 2));
+end
+
